c7de26f176f85c28c2591d1b4497a04351dc5ebf
okcorn <jsok79now@gmail.com>
2024-07-31 01:27:56 +0900

    feat: Add update, delete, tab logics

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { PostProps } from "./PostList";
-import { doc, getDoc } from "firebase/firestore";
+import { deleteDoc, doc, getDoc } from "firebase/firestore";
 import { db } from "firebaseApp";
 import Loader from "./Loader";
+import { toast } from "react-toastify";
 
 export default function PostDetail() {
   const { id } = useParams();
   const [post, setPost] = useState<PostProps | null>(null);
+  const navigate = useNavigate();
 
   const getPost = async (id: string) => {
     const docRef = doc(db, "posts", id);
@@ -15,7 +17,15 @@ export default function PostDetail() {
     setPost({ ...(docSnap.data() as PostProps), id: docSnap.id });
   };
 
-  const handleDelete = () => {};
+  const handleDelete = async () => {
+    const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+    if (confirm && post) {
+      await deleteDoc(doc(db, "posts", post.id));
+      toast.success("게시글을 삭제했습니다.");
+
+      navigate("/");
+    }
+  };
 
   useEffect(() => {
     if (id) {
@@ -34,6 +44,9 @@ export default function PostDetail() {
             <div className="post__date">{post.createdAt}</div>
           </div>
           <div className="post__utils-box">
+            {post.category && (
+              <div className="post__category">{post.category}</div>
+            )}
             <div className="post__delete" onClick={handleDelete}>
               삭제
             </div>
