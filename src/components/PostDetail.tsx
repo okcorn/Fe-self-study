aa73e8faec0575a7257b2b200bbaeb3895684ab9
okcorn <jsok79now@gmail.com>
2024-08-02 00:12:55 +0900

    feat: Add comments features

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -5,6 +5,7 @@ import { deleteDoc, doc, getDoc } from "firebase/firestore";
 import { db } from "firebaseApp";
 import Loader from "./Loader";
 import { toast } from "react-toastify";
+import Comments from "./Comments";
 
 export default function PostDetail() {
   const { id } = useParams();
@@ -36,26 +37,31 @@ export default function PostDetail() {
   return (
     <div className="post__detail">
       {post ? (
-        <div className="post__box">
-          <div className="post__title">{post.title}</div>
-          <div className="post__profile-box">
-            <div className="post__profile" />
-            <div className="post__author-name">{post.email}</div>
-            <div className="post__date">{post.createdAt}</div>
-          </div>
-          <div className="post__utils-box">
-            {post.category && (
-              <div className="post__category">{post.category}</div>
-            )}
-            <div className="post__delete" onClick={handleDelete}>
-              삭제
+        <>
+          <div className="post__box">
+            <div className="post__title">{post.title}</div>
+            <div className="post__profile-box">
+              <div className="post__profile" />
+              <div className="post__author-name">{post.email}</div>
+              <div className="post__date">{post.createdAt}</div>
             </div>
-            <div className="post__edit">
-              <Link to={`/posts/edit/${post.id}`}>수정</Link>
+            <div className="post__utils-box">
+              {post.category && (
+                <div className="post__category">{post.category}</div>
+              )}
+              <div className="post__delete" onClick={handleDelete}>
+                삭제
+              </div>
+              <div className="post__edit">
+                <Link to={`/posts/edit/${post.id}`}>수정</Link>
+              </div>
+            </div>
+            <div className="post__text post__text--pre-wrap">
+              {post?.content}
             </div>
           </div>
-          <div className="post__text post__text--pre-wrap">{post?.content}</div>
-        </div>
+          <Comments post={post} getPost={getPost} />
+        </>
       ) : (
         <Loader />
       )}
